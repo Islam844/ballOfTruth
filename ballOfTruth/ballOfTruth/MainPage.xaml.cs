1a2a4d6f288e71e43fcc4add4c71ee9daf2701d6
unknown <islam4ik2002@mail.ru>
2022-02-01 03:55:51 +0600

    added head, button, label

---
+++ ballOfTruth/ballOfTruth/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ballOfTruth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+        void onButtonClicked(object sender, EventArgs e)
+        {
+            trueOrFalse_Label.Text = rand.Next()%2==0?"Правда":"Ложь";
+        }
+
+        void onButtonPressed(object sender, EventArgs e)
+        {
+            button.Source = "Component1Clicked.png";
+        }
+        void onButtonReleased(object sender, EventArgs e)
+        {
+            button.Source = "Component1.png";
+        }
+    }
+}
